197500e7a8c5bcd64658e3a2514204245d30fe6f
momes <mo.enokida@gmail.com>
2022-03-13 19:21:31 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import mediaIcon from './icon.png';
+import startButton from './start.png';
+import DiscoGremlinFont from './disco-gremlin.png';
+import Misc from './misc.png';
+import React, { useState } from 'react';
+import DragMove from "./DragMove";
+import MediaPlayer from './MediaPlayer';
 function App() {
+  const [translate, setTranslate] = useState({
+    x: 0,
+    y: 0
+  });
+  const [openPlayer, setOpenPlayer] = useState(false);
+
+  const togglePlayer = () => {
+    console.log('clicked');
+    setOpenPlayer(!openPlayer);
+  }
+
+  const handleDragMove = (e) => {
+    setTranslate({
+      x: translate.x + e.movementX,
+      y: translate.y + e.movementY
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {openPlayer && <MediaPlayer onClickClose={togglePlayer} />}
+      <DragMove onDragMove={handleDragMove}>
+          <button
+            className='MediaIconButton'
+            onClick={togglePlayer}
+            style={{
+              transform: `translateX(${translate.x}px) translateY(${translate.y}px)`
+            }}
+          >
+            <img src={mediaIcon} className="MediaIcon"/>
+            <br/>
+            <img src={DiscoGremlinFont} className='Label'/>
+          </button>
+        </DragMove>
+      <div className="footer">
+        <div className='bottomBar'>
+        <img src={startButton} className='StartButton'/>
+        <img src={Misc} className='Misc'/>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/MediaPlayer.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './MediaPlayer.css'; 
+import Close from './close.png';
+import Player from './player.png';
+import ButtonPlay from './button-play.png';
+import ButtonStop from './button-stop.png';
+import ButtonPause from './button-pause.png';
+import ButtonForward from './button-forward.png';
+import ButtonBack from './button-back.png';
+import Bar from './bar.png';
+import AudioURL from './disco_gremlins_01.mp3';
+
+function MediaPlayer({onClickClose}) {
+  const [isPlaying, toggleIsPlaying] = useState(true);
+  const audio = useRef()
+
+  useEffect(() =>{
+    !isPlaying ? audio.current.pause() : audio.current.play();
+  }, [isPlaying])
+
+  const handleClosePlayer = () => {
+    onClickClose();
+  }
+
+  const onClick = (toggleState) => {
+    console.log({isPlaying})
+    console.log({toggleState})
+    if (toggleState === 'stop' || toggleState === 'restart') {
+      audio.current.currentTime = 0;
+    }
+    const toggleBool = (toggleState === 'play' || toggleState === 'restart') ? true : false;
+    if (isPlaying !== toggleBool) {
+    toggleIsPlaying(toggleBool)}
+  }
+  return (
+    <div className='MediaPlayer'>
+      <div className='top'>
+        <button className='CloseButton' onClick={handleClosePlayer}>
+        <img src={Close} className='Close'/>
+        </button>
+      </div>
+      <img src={Player} className='Player' />
+      <img src={Bar} className='Bar'/>
+      <div className='buttons'>
+      <button className='Button' onClick={() => onClick('play')}>
+        <img src={ButtonPlay} className='MediaButton'/>
+      </button>
+      <button className='Button' onClick={() => onClick('stop')}>
+        <img src={ButtonStop} className='MediaButton'/>
+      </button>
+      <button className='Button' onClick={() => onClick('pause')}>
+        <img src={ButtonPause} className='MediaButton'/>
+      </button>
+      <button className='Button' onClick={() => onClick('restart')}>
+        <img src={ButtonBack} className='MediaButton'/>
+      </button>
+      <button className='Button' onClick={() => onClick('restart')}>
+        <img src={ButtonForward} className='MediaButton'/>
+      </button>
+      </div>
+      <audio ref={audio} src={AudioURL}/>
+    </div>
+  )
+} 
+
+export default MediaPlayer;
